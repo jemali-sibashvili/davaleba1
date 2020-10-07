06b958d882ef81b146720eb32670291c2dc36fd1
jemali-sibashvili <72436829+jemali-sibashvili@users.noreply.github.com>
2020-10-07 11:11:03 +0400

    Update B.java

--- java/src/B.java
+++ java/src/B.java
@@ -10,9 +10,9 @@ class B extends A{
 
 
     public void dav_2_2_method_1() {
-        Scanner s = new Scanner(System.in);
+        Scanner n = new Scanner(System.in);
         System.out.println("შეიტანე y: ");
-        y = s.nextInt();
+        y = n.nextInt();
     }
 
     public void dav_2_2_method_2() {
